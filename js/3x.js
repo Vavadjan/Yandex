2c394a5b4c0928bbaed16e18cb4787e53076f7e4
Vavadjan <vavadjan@gmail.com>
2023-10-05 18:43:49 +0300

    !

---
+++ js/3x.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('.body');
+const layer1 = document.querySelector('.layer1');
+const layer2 = document.querySelector('.layer2');
+const layer3 = document.querySelector('.layer3');
+
+container.addEventListener('mousemove', (e) => {
+    const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+
+    layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+    layer2.style.transform = `translate(${xAxis * 2}px, ${yAxis * 2}px)`;
+    layer3.style.transform = `translate(${xAxis * 3}px, ${yAxis * 3}px)`;
+});
