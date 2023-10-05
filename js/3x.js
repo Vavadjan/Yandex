cd3e16ea7940758ccb795f7626f7f64108da0c15
Vavadjan <vavadjan@gmail.com>
2023-10-05 21:34:03 +0300

    xxx

--- js/3x.js
+++ js/3x.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
-    const layer1 = document.querySelector('.layer1');
+    //const layer1 = document.querySelector('.layer1');
     const layer2 = document.querySelector('.layer2');
     const layer3 = document.querySelector('.layer3');
 
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
         const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
 
-        layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+        //layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
         layer2.style.transform = `translate(${xAxis * 2}px, ${yAxis * 2}px)`;
         layer3.style.transform = `translate(${xAxis * 3}px, ${yAxis * 3}px)`;
     });
--- js/dist/3x.dev.js
+++ js/dist/3x.dev.js
@@ -1,14 +1,14 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  var body = document.querySelector('body');
-  var layer1 = document.querySelector('.layer1');
+  var body = document.querySelector('body'); //const layer1 = document.querySelector('.layer1');
+
   var layer2 = document.querySelector('.layer2');
   var layer3 = document.querySelector('.layer3');
   body.addEventListener('mousemove', function (e) {
     var xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    var yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    layer1.style.transform = "translate(".concat(xAxis, "px, ").concat(yAxis, "px)");
+    var yAxis = (window.innerHeight / 2 - e.pageY) / 25; //layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+
     layer2.style.transform = "translate(".concat(xAxis * 2, "px, ").concat(yAxis * 2, "px)");
     layer3.style.transform = "translate(".concat(xAxis * 3, "px, ").concat(yAxis * 3, "px)");
   });
