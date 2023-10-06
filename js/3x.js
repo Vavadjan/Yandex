e810ca099144cddb3161159576ea1e1a4b7c8840
Vavadjan <vavadjan@gmail.com>
2023-10-06 17:39:48 +0300

    Oh

--- js/3x.js
+++ js/3x.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
         const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
 
-        //layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+        layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
         layer2.style.transform = `translate(${xAxis * 2}px, ${yAxis * 2}px)`;
         layer3.style.transform = `translate(${xAxis * 3}px, ${yAxis * 3}px)`;
     });
--- js/dist/3x.dev.js
+++ js/dist/3x.dev.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var layer3 = document.querySelector('.layer3');
   body.addEventListener('mousemove', function (e) {
     var xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    var yAxis = (window.innerHeight / 2 - e.pageY) / 25; //layer1.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
-
+    var yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    layer1.style.transform = "translate(".concat(xAxis, "px, ").concat(yAxis, "px)");
     layer2.style.transform = "translate(".concat(xAxis * 2, "px, ").concat(yAxis * 2, "px)");
     layer3.style.transform = "translate(".concat(xAxis * 3, "px, ").concat(yAxis * 3, "px)");
   });
